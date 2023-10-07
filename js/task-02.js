a0df911a42a405ebf6bb6ebead58fad378da126b
Anna-Chernukhina <sakuretka27@gmail.com>
2023-10-07 11:50:59 +0100

    task 02 complete

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,22 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector("ul");
+
+function addList(array) {
+  const liList = [];
+
+  array.forEach(elem => {
+    // console.log(elem);
+    const li = document.createElement("li");
+    li.textContent = elem;
+    li.classList = "item";
+    liList.push(li);
+  });
+
+  // добавляем элементы списка в DOM
+  list.append(...liList);
+}
+
+addList(ingredients);
