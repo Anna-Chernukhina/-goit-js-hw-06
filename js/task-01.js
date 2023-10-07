3a1e34086685eb9d57ed9a8a891dbae144dfaaec
Anna-Chernukhina <sakuretka27@gmail.com>
2023-10-07 11:32:50 +0100

    task 01 complete

--- js/task-01.js
+++ js/task-01.js
@@ -1,5 +1,13 @@
-const refsItems = document.querySelectorAll('.item');
 
-console.log(refsItems);
+const items = document.querySelectorAll('.item');
+const itemsCount = items.length;
+console.log(`Number of categories: ${itemsCount}`);
 
+items.forEach(item => {
+    const categoryName = (item.querySelector('h2').textContent);
+    const subCategoryCount = item.lastElementChild.children.length;
+
+    console.log(`Category: ${categoryName}`);
+    console.log(`Elements: ${subCategoryCount}`);
+});
 
