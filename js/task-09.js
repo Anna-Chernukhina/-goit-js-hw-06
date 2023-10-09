189b1acc9df1a2c0cc8bbfb78afe56d081e2d121
Anna-Chernukhina <sakuretka27@gmail.com>
2023-10-09 10:58:35 +0100

    task 09 complete

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btn = document.querySelector("button");
+const span = document.querySelector("span");
+const body = document.querySelector("body");
+
+btn.addEventListener('click', () => {
+  const newColor = getRandomHexColor();
+  span.textContent = newColor;
+  body.style.backgroundColor = newColor;
+})
