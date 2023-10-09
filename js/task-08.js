2561dd5388e2abb548ed5e598965268d1a12dc56
Anna-Chernukhina <sakuretka27@gmail.com>
2023-10-09 10:50:08 +0100

    task 08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", check);
 
+function check(event) {
+    event.preventDefault();
+    
+    const formData = {};
+    const formTarget = event.currentTarget;
+    const userEmail = formTarget.elements.email.value;
+    const userPassword = formTarget.elements.password.value;
+
+    if (userEmail !== "" && userPassword !== "") {
+        formData.email = userEmail;
+        formData.password = userPassword;
+        console.log(formData);
+    }
+
+    else {
+        alert("Please fill empty fields!");
+    }
+
+    formTarget.reset();
+}
